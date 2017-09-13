73612460c6efdf807e334ff3380e7777e8c14874
Syed W Shah <31411311+syedshah94@users.noreply.github.com>
2017-09-12 21:06:18 -0400

    Add files via upload

---
+++ Final_Project/product_pages/slideShow.js
@@ -0,0 +1,24 @@
+let slideIndex= 1;
+showDivs(slideIndex);
+
+function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n){
+    let classArray =document.getElementsByClassName("homeImage");
+    if(n > classArray.length) {slideIndex=1}
+    if(n < 1) {slideIndex=classArray.length}    
+    for(let i = 0; i<classArray.length; i++){
+        classArray[i].style.display="none";
+    }
+    classArray[slideIndex-1].style.display ="block";
+}
+
+
+// //Side bar AKA Nav bar
+// $(document).ready(function() {
+//     $('#sidebarButton').click(function(){
+//         $('#sidebar').toggleClass('visible');
+//     });
+// });
