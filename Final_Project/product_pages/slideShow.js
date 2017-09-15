352bf685a9f067bfa0517c38803575f36984588c
Syed W Shah <31411311+syedshah94@users.noreply.github.com>
2017-09-15 09:23:47 -0400

    Add files via upload

--- Final_Project/product_pages/slideShow.js
+++ Final_Project/product_pages/slideShow.js
@@ -1,18 +1,20 @@
-let slideIndex= 1;
+let slideIndex= 1; //default index
 showDivs(slideIndex);
 
-function plusDivs(n){
-    showDivs(slideIndex += n);
+//direction will be either 1 or -1
+//to be called on click of directional arrows on slideshow
+function plusDivs(direction) {
+    showDivs(slideIndex += direction);
 }
 
-function showDivs(n){
+function showDivs(direction){
     let classArray =document.getElementsByClassName("homeImage");
-    if(n > classArray.length) {slideIndex=1}
-    if(n < 1) {slideIndex=classArray.length}    
+    if(direction > classArray.length) {slideIndex=1} //For when you go past array length
+    if(direction < 1) {slideIndex=classArray.length} //For when you go "before" array length   
     for(let i = 0; i<classArray.length; i++){
         classArray[i].style.display="none";
     }
-    classArray[slideIndex-1].style.display ="block";
+    classArray[slideIndex-1].style.display ="block"; //array indexed at "0", use -1 to resolve
 }
 
 
